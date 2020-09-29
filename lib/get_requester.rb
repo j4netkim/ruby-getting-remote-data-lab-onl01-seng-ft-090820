028cf118fd752362ae8d32cbf0c59635d459c836
J K <kj688895@gmail.com>
2020-09-29 16:41:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,7 +4,11 @@ require 'json'
 
 class GetRequester
   attr_accessor :url
-  
+
+  def initialize(url)
+    @url = url
+  end 
+
   def get_response_body
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
